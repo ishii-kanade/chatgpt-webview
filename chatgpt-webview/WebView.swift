4cc4e3a604877fdffc47a95954c41200bbfc0c5c
Ishii Kanade <ishii-kanade@laamile.com>
2024-02-12 03:29:56 +0900

    とりあえずウェブページを表示する機能の実装

--- chatgpt-webview/WebView.swift
+++ chatgpt-webview/WebView.swift
@@ -5,4 +5,20 @@
 //  Created by ishii kanade on 2024/02/12.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: NSViewRepresentable {
+    let url: URL
+
+    func makeNSView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        nsView.load(request)
+    }
+}
+
+
