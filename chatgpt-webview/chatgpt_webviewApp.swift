d3de0a816b91ad0f09f3d2962183b99d999fe4c7
Ishii Kanade <ishii-kanade@laamile.com>
2024-02-12 03:07:21 +0900

    Initial Commit

---
+++ chatgpt-webview/chatgpt_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatgpt_webviewApp.swift
+//  chatgpt-webview
+//
+//  Created by ishii kanade on 2024/02/12.
+//
+
+import SwiftUI
+
+@main
+struct chatgpt_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
